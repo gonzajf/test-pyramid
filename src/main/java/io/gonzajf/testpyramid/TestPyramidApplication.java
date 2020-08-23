00d49ca4b820bf41c682dda13e6aa6b488781b75
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-23 19:53:49 -0300

    initial structure of project

---
+++ src/main/java/io/gonzajf/testpyramid/TestPyramidApplication.java
@@ -0,0 +1,13 @@
+package io.gonzajf.testpyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPyramidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPyramidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gonzajf/testpyramid/TestPyramidApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gonzajf.testpyramid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPyramidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
