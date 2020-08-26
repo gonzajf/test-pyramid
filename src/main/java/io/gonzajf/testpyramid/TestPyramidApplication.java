47f2194898b65b3454fb012966250fa19706cd85
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-26 15:49:45 -0300

    added resttemplate as bean

--- src/main/java/io/gonzajf/testpyramid/TestPyramidApplication.java
+++ src/main/java/io/gonzajf/testpyramid/TestPyramidApplication.java
@@ -2,10 +2,18 @@ package io.gonzajf.testpyramid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TestPyramidApplication {
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplateBuilder().build();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestPyramidApplication.class, args);
 	}
