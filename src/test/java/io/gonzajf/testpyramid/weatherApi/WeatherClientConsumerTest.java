ccd5f3eb755d771331de025768ff285eb325b095
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-08-28 11:47:32 -0300

    change in weather api consumer test

--- src/test/java/io/gonzajf/testpyramid/weatherApi/WeatherClientConsumerTest.java
+++ src/test/java/io/gonzajf/testpyramid/weatherApi/WeatherClientConsumerTest.java
@@ -1,36 +1,39 @@
 package io.gonzajf.testpyramid.weatherApi;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.ResourceUtils;
 
-import com.jayway.jsonpath.JsonPath;
-
-import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import io.gonzajf.testpyramid.weatherapi.WeatherClient;
+import io.gonzajf.testpyramid.weatherapi.WeatherResponse;
 
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(providerName = "weatherApiProvider", port = "8089")
+@SpringBootTest
 public class WeatherClientConsumerTest {
+	
+	@Autowired
+	private WeatherClient weatherClient;
 	
 	 @Pact(consumer="test_consumer")
 	 public RequestResponsePact createPact(PactDslWithProvider builder) throws IOException {
         return builder
                 .given("weather forecast data")
-                .uponReceiving("a request for a weather request for Berlin")
+                .uponReceiving("A request for a weather request for Berlin")
 	                .path("/weather")
                 	.method("GET")
                 	.query("q=berlin&appid=bab965417f0e42ccc4a88def77e0b00c")
@@ -39,18 +42,14 @@ public class WeatherClientConsumerTest {
                     .body(FileLoader.read("classpath:__files/weatherApiResponse.json"),
                             ContentType.APPLICATION_JSON)
                 .toPact();
-	    }
+	   }
 
 	 @Test
 	 @PactTestFor(pactMethod = "createPact")
-	 public void shouldFetchWeatherInformation(MockServer mockServer) throws IOException {
+	 public void shouldFetchWeatherInformation() throws IOException {
 		
-		 HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/weather?q=berlin&appid=bab965417f0e42ccc4a88def77e0b00c")
-				 						.execute()
-				 						.returnResponse();
-		
-		 assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
-		 assertThat(JsonPath.read(httpResponse.getEntity().getContent(), "$.weather[0].main").toString()).isEqualTo("Rain");
+		 WeatherResponse response = weatherClient.getWeatherByCityName("berlin");
+		 assertEquals(response.getWeather().get(0).getMain(), "Rain");
 	 }
 	 
 
